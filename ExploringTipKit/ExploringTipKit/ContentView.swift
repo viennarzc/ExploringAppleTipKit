219626d6f1745ae9a614d575532d37c0da642641
Viennarz Curtiz <viennarzc@gmail.com>
2023-07-08 07:09:45 +0800

    Create project

---
+++ ExploringTipKit/ExploringTipKit/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  ExploringTipKit
+//
+//  Created by Viennarz Curtiz on 7/8/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ ExploringTipKit/ExploringTipKit/ExploringTipKitApp.swift
@@ -0,0 +1,17 @@
+//
+//  ExploringTipKitApp.swift
+//  ExploringTipKit
+//
+//  Created by Viennarz Curtiz on 7/8/23.
+//
+
+import SwiftUI
+
+@main
+struct ExploringTipKitApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
